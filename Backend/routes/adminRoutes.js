13efcf33e03a72282f4a310e4785f3b800317d87
lee-lgp-solutions <lee@lgp.solutions>
2024-03-26 18:27:18 +0000

    Auths on admin screen

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const { fetchAllGrants, updateStatus } = require("../controllers/grantControllers")
+const { fetchAllAuths } = require("../controllers/authControllers")
+
 const requireAuth = require("../middleware/requireAuth")
 
 const router = express.Router()
@@ -7,6 +9,9 @@ router.use(requireAuth)
 
 // setup routes and controller functions
 router.get('/admin', fetchAllGrants)
+
+// this needs to be here to do auth and cast to json
+router.get("/auth", fetchAllAuths);
 
 router.patch('/admin/updateStatus', updateStatus)
 
