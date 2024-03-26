1eb6778d2efa206201a08fa5791f7a82b97ff72a
lee-lgp-solutions <lee@lgp.solutions>
2024-03-26 18:54:49 +0000

    Auths on admin screen

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -10,7 +10,7 @@ router.use(requireAuth)
 router.get('/admin', fetchAllGrants)
 
 // this needs to be here to do auth and cast to json
-router.get("/auth", fetchAllAuths);
+router.get("/admin/auths", fetchAllAuths);
 
 router.patch('/admin/updateStatus', updateStatus)
 
