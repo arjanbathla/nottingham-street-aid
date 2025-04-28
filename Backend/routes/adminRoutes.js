2f5beed294d69c690cdc18dda055b70e1457e596
T0414208 <mohamed.nouadi2022@my.ntu.ac.uk>
2025-04-28 09:14:48 +0100

    NSA61

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { fetchAllAuths, fetchAllGrants, updateStatus } = require("../controllers/grantControllers")
+const { fetchAllAuths, fetchAllGrants, updateStatus, deleteGrant} = require("../controllers/grantControllers")
 
 const requireAuth = require("../middleware/requireAuth")
 
@@ -11,6 +11,8 @@ router.get('/admin', fetchAllGrants)
 
 // this needs to be here to do auth and cast to json
 router.get("/admin/auths", fetchAllAuths);
+// Route to delete a grant
+router.delete('/controllers/grantControllers', deleteGrant);
 
 router.patch('/admin/updateStatus', updateStatus)
 
